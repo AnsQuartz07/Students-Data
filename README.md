a3aac27ee1c97811844f1d148c30676aa2b189ff
anshukumar <anshu.kumar@dresma.com>
2022-05-30 22:57:51 +0530

    Join with marksheet collection

--- README.md
+++
@@ -1,2 +0,0 @@
-# Students-Data
-GET, POST, Delete and PUT requests are implemented using Node.JS, Express.JS and MongoDB
--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require('express'); // importing express
-const app = express();  // importing express
+const app = express();  // importing expres
 const dotenv = require('dotenv');  //importing Dotenv
 const mongoose = require('mongoose') // importing MongoDB
 
 // importing routes
-const studRoutes = require('./stud');
+const studRoutes = require('./Routes/data');
+const marksRoutes = require('./Routes/marks');
+const joinRoutes = require('./Routes/Join')
 
 dotenv.config();  //configure Dotenv
 
 //connect to database
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true,useUnifiedTopology: true }, () => 
+mongoose.connect(process.env.DB_CONNECT,  () => 
 console.log('connected to database!')
-);
+)
 
 //middleware
 app.use(express.json());
+
+
 // route middleware
 app.use('/api/stud',studRoutes); // It will connect to studRoutes.
+app.use('/api/marks',marksRoutes);
+app.use('/api/join',joinRoutes);
 
 // Connect to server
 const port = 8080;
